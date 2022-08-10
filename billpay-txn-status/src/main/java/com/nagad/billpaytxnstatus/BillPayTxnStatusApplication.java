14612131251d174d9d55b54bf70d48286e921d93
ridoy19 <jh_ridoy@yahoo.com>
2022-08-10 09:33:05 +0600

    starter initial commit

---
+++ billpay-txn-status/src/main/java/com/nagad/billpaytxnstatus/BillPayTxnStatusApplication.java
@@ -0,0 +1,15 @@
+package com.nagad.billpaytxnstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BillPayTxnStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillPayTxnStatusApplication.class, args);
+	}
+
+}
